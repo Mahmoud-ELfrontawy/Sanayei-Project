b3bd0a4465c347f0c23567a42432ca278d1a0683
Mahmoud-ELfrontawy <123123mahmoudmohamed@gmail.com>
2025-07-11 20:04:26 +0300

    add section all sanayei + change responsive header and all sanayei

---
+++ src/js/main.js
@@ -0,0 +1,56 @@
+// Mobile Menu
+
+    const toggleBtn = document.getElementById('menu-toggle');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    toggleBtn.addEventListener('click', () => {
+    if(mobileMenu.classList.contains("hidden")){
+        mobileMenu.classList.remove('hidden');
+        mobileMenu.classList.add('flex');
+
+    }else{
+        mobileMenu.classList.remove('flex');
+        mobileMenu.classList.add('hidden');
+    }
+    
+});
+
+
+// change color heart
+
+const hearts = document.querySelectorAll(".heartIcon");
+
+hearts.forEach(heartIcon => {
+    heartIcon.addEventListener("click", () => {
+        if (heartIcon.classList.contains("text-red-500")) {
+            heartIcon.classList.remove("text-red-500");
+            heartIcon.classList.add("text-gray-600");
+        } else {
+            heartIcon.classList.remove("text-gray-600");
+            heartIcon.classList.add("text-red-500");
+        }
+    });
+});
+
+
+// check stars
+const cards = document.querySelectorAll(".star");
+
+cards.forEach(card => {
+    const stars = card.querySelectorAll(".stars i");
+
+    stars.forEach((star, index) => {
+        star.addEventListener("click", () => {
+            stars.forEach((s, i) => {
+                if (i <= index) {
+                    s.classList.add("fas");
+                    s.classList.remove("far");
+                } else {
+                    s.classList.add("far");
+                    s.classList.remove("fas");
+                }
+            });
+        });
+    });
+});
+
