5889ea7813008d0b8e21bbeacf8650c6fb2b11d2
Mahmoud-ELfrontawy <123123mahmoudmohamed@gmail.com>
2025-10-17 14:58:04 +0300

    Add Project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="h-lvh flex justify-center
+      items-center text-6xl text-orange-500">صنايعي دوت كوم</h1>
+    </>
+  )
+}
+
+export default App
