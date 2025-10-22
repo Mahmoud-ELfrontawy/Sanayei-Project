f79cfc7904c660b6a361ec8cf2d5365758eb1d2d
Kareem Ahmed <kreem19021902@gmail.com>
2025-10-22 17:50:40 +0300

    My update message

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,9 +5,10 @@ import imageHome from "../sanayei-img/home.jpg";
 import "./Home.css";
 import AboutSection from "./AboutSection";
 import ServicesSection from "./ServicesSection";
-<<<<<<< HEAD
 import CraftsmenSection from "./CraftsmenSection";
 import RequestServiceSection from "./RequestServiceSection";
+
+import Footer from "./Footer";
 
 function Home() {
   return (
@@ -30,31 +31,9 @@ function Home() {
       <ServicesSection />
       <CraftsmenSection />
       <RequestServiceSection />
+      <Footer />
     </section>
   );
-=======
-import Footer from "./Footer";
-
-function Home() {
-    return (
-        <section className="home">
-            <div className="image-home">
-                <img src={imageHome} alt="Home" className="image-home__img" />
-                <div className="image-home__overlay" />
-                <div className="image-home__content">
-                    <h1>اطلب صنايعك... وخلي الشغل علينا!</h1>
-                    <p>مع صنايعي هتلاقي كل خدمات الصيانة والدِيكور في مكان واحد -</p>
-                    <p className="text-white"> صنايعية خبرة، أسعار واضحة، وشغل مضمون يوصل لحد بابك.</p>
-                    <Button/>
-                </div>
-            </div>
-            <CardsRow/>
-            <AboutSection/>
-            <ServicesSection/>
-            <Footer/>
-        </section>
-    );
->>>>>>> 0d5dc4df6d27331a4828776425aa5876ac32e8d7
 }
 
 export default Home;
